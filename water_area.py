7775f9e7413325750127735fbbf399c89d114588
Matvey Kostukovsky <matvelius@gmail.com>
2019-05-30 21:03:20 -0500

    pass all test cases!

--- water_area.py
+++ water_area.py
@@ -1,33 +1,39 @@
-# array = [2, 0, 7, 0, 3, 0, 7] #20
-# array = [0, 1, 0, 1, 0] #1
-# array = [0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3] #48
-array = [0, 8, 0, 0, 10, 0, 0, 10, 0, 0, 1, 1, 0, 3] #49
-# array = [4, 0, 2, 4, 3, 6, 0, 0, 7] #19
-# array = [0, 0, 0, 0, 0]
+heights = [2, 0, 7, 0, 3, 0, 7] #20
+# heights = [0, 100, 0, 0, 10, 1, 1, 10, 1, 0, 1, 1, 0, 0] #39
+##################10 10      9  9         1     
+# heights = [0, 1, 0, 1, 0] #1
+# heights = [0, 1, 0, 0, 0] #0
+# heights = [0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3] #48
+# heights = [0, 8, 0, 0, 10, 0, 0, 10, 0, 0, 1, 1, 0, 3] #49
+# heights = [4, 0, 2, 4, 3, 6, 0, 0, 7] #19
+# heights = [0, 0, 0, 0, 0]
 
-def waterArea(array):
-    # print(f"original array: {array}")
+def waterArea(heights):
+    # print(f"original heights: {heights}")
+
+    if len(heights) == 0:
+        return 0
 
     totalArea = 0
     maxHeight = 0
 
-    # enumerate the original array
-    enumeratedHeightsArray = []
-    for index in range(0, len(array)):
-        enumeratedHeightsArray.append((array[index], index))
-    # print(f"enumeratedHeightsArray: {enumeratedHeightsArray}")
+    # enumerate the original heights
+    enumeratedHeights = []
+    for index in range(0, len(heights)):
+        enumeratedHeights.append((heights[index], index))
+    # print(f"enumeratedHeights: {enumeratedHeights}")
 
 
-    # sorting the array from largest to smallest
-    sortedHeightsArray = sorted(enumeratedHeightsArray, reverse = True)
-    # print(f"sortedHeightsArray: {sortedHeightsArray}")
+    # sorting the heights from largest to smallest
+    sortedHeights = sorted(enumeratedHeights, reverse = True)
+    # print(f"sortedHeights: {sortedHeights}")
 
     
     # first, find water area between two highest peaks
     
     # 1) figure out which of the first two peaks is leftmost, and which is rightmost
-    leftPeakIndex = sortedHeightsArray[0][1] if sortedHeightsArray[0][1] < sortedHeightsArray[1][1] else sortedHeightsArray[1][1]
-    rightPeakIndex = sortedHeightsArray[0][1] if sortedHeightsArray[0][1] > sortedHeightsArray[1][1] else sortedHeightsArray[0][1]
+    leftPeakIndex = sortedHeights[0][1] if sortedHeights[0][1] < sortedHeights[1][1] else sortedHeights[1][1]
+    rightPeakIndex = sortedHeights[0][1] if sortedHeights[0][1] > sortedHeights[1][1] else sortedHeights[1][1]
     # print(f"leftPeakIndex: {leftPeakIndex}; rightPeakIndex: {rightPeakIndex}")
     
     # COME BACK TO THIS EDGE CASE!!
@@ -37,20 +43,20 @@ def waterArea(array):
         # or perhaps start with a loop that performs this check every time
         
         # 2) figure out which of the first two peaks is lower (that's our max height!)
-        maxHeight = array[leftPeakIndex] if array[leftPeakIndex] < array[rightPeakIndex] else array[rightPeakIndex]
+        maxHeight = heights[leftPeakIndex] if heights[leftPeakIndex] < heights[rightPeakIndex] else heights[rightPeakIndex]
         # print(f"maxHeight: {maxHeight}")
         
         # 3) figure out water area between the two peaks
         
-        totalArea += calculateArea(leftPeakIndex, rightPeakIndex, maxHeight)
+        totalArea += calculateArea(leftPeakIndex, rightPeakIndex, maxHeight, heights)
         # print(f"totalArea is at first: {totalArea}\n")
 
     
     
-    # iterate through the rest of the array to find other peaks,
+    # iterate through the rest of the heights to find other peaks,
     # check if they are to the left or to the right
     # if so, set the new left & right peak indices
-    for (height, index) in sortedHeightsArray[2:]:
+    for (height, index) in sortedHeights[2:]:
         
         #if new peak is left of leftmost peak
         if index < leftPeakIndex:
@@ -59,7 +65,7 @@ def waterArea(array):
             newRightPeakIndex = leftPeakIndex
             leftPeakIndex = index
             
-            totalArea += calculateArea(leftPeakIndex, newRightPeakIndex, maxHeight)
+            totalArea += calculateArea(leftPeakIndex, newRightPeakIndex, maxHeight, heights)
             
             # print(f"leftPeakIndex: {leftPeakIndex}; newRightPeakIndex: {newRightPeakIndex}")
             # print(f"maxHeight is now: {maxHeight}")
@@ -74,7 +80,7 @@ def waterArea(array):
             newLeftPeakIndex = rightPeakIndex
             rightPeakIndex = index
             
-            totalArea += calculateArea(newLeftPeakIndex, rightPeakIndex, maxHeight)
+            totalArea += calculateArea(newLeftPeakIndex, rightPeakIndex, maxHeight, heights)
             
             # print(f"newLeftPeakIndex: {newLeftPeakIndex}; rightPeakIndex: {rightPeakIndex}")
             # print(f"maxHeight is now: {maxHeight}")
@@ -92,14 +98,14 @@ def waterArea(array):
     return totalArea
     
 
-def calculateArea(leftIndex, rightIndex, maxHeight):
+def calculateArea(leftIndex, rightIndex, maxHeight, heights):
     if leftIndex < rightIndex and (leftIndex + 1) <= (rightIndex - 1):
         total = 0
 
         for index in range(leftIndex + 1, rightIndex):
-            if (maxHeight - array[index]) >= 0:
-                total += maxHeight - array[index]
-                # print(f"adding {maxHeight - array[index]} to totalArea")
+            if (maxHeight - heights[index]) >= 0:
+                total += maxHeight - heights[index]
+                # print(f"adding {maxHeight - heights[index]} to totalArea")
 
         return total 
 
@@ -107,5 +113,5 @@ def calculateArea(leftIndex, rightIndex, maxHeight):
     return 0
     
 
-result = waterArea(array)
+result = waterArea(heights)
 print(result)
